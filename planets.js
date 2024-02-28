547a1b7a48ca25ae5515c435218a928dd907ded1
Quenton Barger <qbarger22@gmail.com>
2024-02-28 13:38:38 -0700

    displaying username on each planet page

---
+++ planets.js
@@ -0,0 +1,6 @@
+const userNameEl = document.querySelector('.player-name');
+            userNameEl.textContent = this.getPlayerName();
+
+function getPlayerName() {
+    return localStorage.getItem('username') ?? 'Unkown';
+}
