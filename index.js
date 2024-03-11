287a8c776f3b269d3d0f529449b51e7847ddadbe
Quenton Barger <qbarger22@gmail.com>
2024-03-11 10:53:56 -0600

    First express running

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080);
+app.use(express.json());
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+app.use((_req, res) => {
+    res.sendFile('index.html', {root: 'public'});
+});
+
+app.listen(3000);
