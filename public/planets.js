a1b6d7f3ad5a8b80dfd50a319bc145318d5054db
Quenton Barger <qbarger22@gmail.com>
2024-03-11 20:00:29 -0600

    added deployService.sh

--- public/planets.js
+++ public/planets.js
@@ -1,6 +1,22 @@
+window.onload = async function(data){
+    if(window.location.pathname === '/sun.html'){
+        fetch('https://en.wikipedia.org/wiki/Sun')
+            .then((response) => response.json())
+            .then((data) => {
+                const container = document.querySelector('#sun');
+                const info = document.createElement('s');
+                info.classList.add('sun')
+                info.textContent = data.content;
+                container.append(info);
+            })
+    }
+}
+
 const userNameEl = document.querySelector('.player-name');
-            userNameEl.textContent = this.getPlayerName();
+userNameEl.textContent = this.getPlayerName();
 
 function getPlayerName() {
     return localStorage.getItem('username') ?? 'Unkown';
-}
+};
+
+
