1c1b58b233e3fb75f2d6b97afc19ed8c309b9f9e
Quenton Barger <qbarger22@gmail.com>
2024-03-13 16:35:33 -0600

    fixing spacing in planet pages

--- public/planets.js
+++ public/planets.js
@@ -1,4 +1,8 @@
-
+window.onload = async function(data){
+    if(window.location.pathname === "/sun.html"){
+        localStorage.setItem('')
+    }
+}
 
 const userNameEl = document.querySelector('.player-name');
 userNameEl.textContent = this.getPlayerName();
