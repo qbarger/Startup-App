eb28f7caedf61bf48391ed52ba1f4944776149d0
Quenton Barger <qbarger22@gmail.com>
2024-04-05 10:49:26 -0600

    setup peerProxy.js

---
+++ peerProxy.js
@@ -0,0 +1,48 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
+function peerProxy(httpServer){
+    const wss = new WebSocketServer({ noServer: true });
+
+    httpServer.on('upgrade', (request,socket,head) => {
+        wss.handleUpgrade(request,socket,head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
+    });
+
+    let connections = {};
+
+    wss.on('connection', (ws) => {
+        const connection = { id: uuid.v4(), alive: true, ws: ws };
+        connections.push(connection);
+
+        ws.on('message', function message(data) {
+            connections.forEach((element) => {
+                if(element.id !== connection.id){
+                    element.ws.send(data);
+                }
+            });
+        });
+
+        ws.on('close', () => {
+            const pos = connections.findIndex((o, i) => o.id === connection.id);
+
+            if(pos >= 0) {
+                connections.splice(pos, 1);
+            }
+        })
+    });
+
+    setInterval(() => {
+        connections.forEach((element) => {
+            if(!element.alive){
+                element.ws.terminate();
+            } else {
+                element.alive = false;
+                element.ws.ping();
+            }
+        });
+    }, 1000);
+}
+
+module.exports = { peerProxy };
