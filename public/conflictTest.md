42ac6b9b52321b13e8957d948b8f4921ea65df21
Quenton Barger <qbarger22@gmail.com>
2024-04-17 18:03:30 -0600

    running vite

--- public/conflictTest.md
+++
@@ -1,5 +0,0 @@
-# Conflict Test File
-
-##  Creating Conflict
-
-### I believe I can fly...I believe I can touch the sky! 
--- public/planets.js
+++
@@ -1,8 +0,0 @@
-const userNameEl = document.querySelector('.player-name');
-userNameEl.textContent = this.getPlayerName();
-
-function getPlayerName() {
-    return localStorage.getItem('username') ?? 'Unkown';
-};
-
-
